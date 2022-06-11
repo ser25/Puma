a1ed245a3085d18a05be783f263e01def157b8ea
ser25 <shatsovniy25@gmail.com>
2022-06-11 22:34:41 +0300

    Main and slider

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const swiper = new Swiper('.main__slider', {
+    speed: 500,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+  });
--- js/burger-menu.js
+++ js/burger-menu.js
@@ -1,13 +1,28 @@
 //меню бургер 
-
 const iconMenu = document.querySelector('.menu__icon');
 const fillLogo = document.querySelector('.path-burger')//зміна коліра в Logo при бургер меню
+const navBar = document.querySelector('.menu__body');
+const menuLinks = document.querySelectorAll('.menu__link');
+const headerButton = document.querySelectorAll('.header-button');
 if (iconMenu){
-  const navBar = document.querySelector('.menu__body');
   iconMenu.addEventListener("click", function(e){
     document.body.classList.toggle('_lock');
     iconMenu.classList.toggle('_active');
     navBar.classList.toggle('_active');
     fillLogo.classList.toggle('_fillWhite')
   });
+}
+if(menuLinks.length > 0 || headerButton){//при кліке на силку виходить з бургер меню 
+  menuLinks.forEach(menuLink =>{
+    menuLink.addEventListener("click", function(e){
+      const menuLink = e.target
+      if (iconMenu.classList.contains("._active")) {
+        document.body.classList.remove('_lock');
+        iconMenu.classList.remove('_active');
+        navBar.classList.remove('_active');
+        fillLogo.classList.remove('_fillWhite') 
+      }
+    });
+  });
+
 }
