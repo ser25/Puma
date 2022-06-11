9fed1d18c56921ce47ea52d0f822417152708640
ser25 <shatsovniy25@gmail.com>
2022-06-11 11:50:57 +0300

    Fixt Logo Fill

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -1,11 +1,13 @@
 //меню бургер 
 
 const iconMenu = document.querySelector('.menu__icon');
+const fillLogo = document.querySelector('.path-burger')//зміна коліра в Logo при бургер меню
 if (iconMenu){
   const navBar = document.querySelector('.menu__body');
   iconMenu.addEventListener("click", function(e){
     document.body.classList.toggle('_lock');
     iconMenu.classList.toggle('_active');
     navBar.classList.toggle('_active');
+    fillLogo.classList.toggle('_fillWhite')
   });
 }
