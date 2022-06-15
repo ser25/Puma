e1d8cdd1d5955b94bd2792f4abb3417fa3b9196f
ser25 <shatsovniy25@gmail.com>
2022-06-15 19:51:45 +0300

    Gallary

--- js/app.js
+++ js/app.js
@@ -5,5 +5,33 @@ const swiper = new Swiper('.main__slider', {
         prevEl: '.swiper-button-prev',
       },
   });
+  const swiperGallary = new Swiper('.gallary__slider', {
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 0,
+      },
+      460: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      580:{
+        slidesPerView: 3,
+        spaceBetween: 10,
+        
+      },
+      1200: {
+        slidesPerView: 4,
+        spaceBetween: 30,
+      }
+    },
+    speed: 500,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+  });
 
 
